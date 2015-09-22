1e1cb3a4dbac40ad8ae87f49cfada6c84401b824
Mochamad Fazar <mochamad.fazar@gmail.com>
2015-09-22 12:23:06 +0700

    view

---
+++ DSLNG.PEAR/DSLNG.PEAR.Web/Controllers/CalculatorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DSLNG.PEAR.Web.ViewModels.Calculator;
+
+namespace DSLNG.PEAR.Web.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public ActionResult Index()
+        {
+            var viewModel = new CalculatorIndexViewModel();
+            return View(viewModel);
+        }
+	}
+}
