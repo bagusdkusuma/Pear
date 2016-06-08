4ef5cd85f0ab9204726ed57ca71a07d945180a4b
Alan <alan.aprianto@gmail.com>
2016-06-08 09:56:54 +0700

    kpi configuration => aggregation formula, styling

--- DSLNG.PEAR/DSLNG.PEAR.Web/ViewModels/Kpi/UpdateKpiViewModel.cs
+++ DSLNG.PEAR/DSLNG.PEAR.Web/ViewModels/Kpi/UpdateKpiViewModel.cs
@@ -70,9 +70,13 @@ namespace DSLNG.PEAR.Web.ViewModels.Kpi
 
         public YtdFormula YtdFormula { get; set; }
         [Required]
-        [Display(Name = "PMS Formula Ytd")]
+        [Display(Name = "Aggregation Formula")]
         public string YtdFormulaValue { get; set; }
         public IEnumerable<SelectListItem> YtdFormulaList { get; set; }
+
+
+        [Display(Name = "Custom Formula")]
+        public string CustomFormula { get; set; }
 
         [Display(Name="Measurement")]
         [Required]
