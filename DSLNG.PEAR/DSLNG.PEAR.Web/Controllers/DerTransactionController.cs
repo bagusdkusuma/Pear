7ae5a6826a1453607f1e17d240e847c9647f809d
Mochamad Fazar <mochamad.fazar@gmail.com>
2016-07-25 18:05:21 +0700

    add form input

---
+++ DSLNG.PEAR/DSLNG.PEAR.Web/Controllers/DerTransactionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DSLNG.PEAR.Web.Controllers
+{
+    public class DerTransactionController : Controller
+    {
+        // GET: DerTransaction
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Input()
+        {
+            return View();
+        }
+
+    }
+}
