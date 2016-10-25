0b940c5065d7544bd52fd6da33c34891d347de6d
bagusdk <bagusdk@gmail.com>
2016-10-26 00:18:53 +0700

    re-entry default value

--- DSLNG.PEAR/DSLNG.PEAR.Web/Helpers/HtmlHelpers.cs
+++ DSLNG.PEAR/DSLNG.PEAR.Web/Helpers/HtmlHelpers.cs
@@ -685,7 +685,7 @@ namespace DSLNG.PEAR.Web.Helpers
             value = valueObject.Value;
             id = valueObject.ExistValue == "now" ? valueObject.Id : 0;
             existValue = valueObject.ExistValue;
-            return new MvcHtmlString(string.Format("<input type=\"text\" value=\"{0}\" class=\"der-value-{1} form-control der-kpi {8}\"   placeholder=\"{2}\" tabindex=\"{3}\" data-type=\"{4}\" data-kpi-id=\"{5}\" data-id=\"{6}\" data-value-type=\"{7}\" style=\"width: {8}px; text-align: right; \" />", value, existValue, placeholder, tabIndex, type, kpiId, id, valueType, additionalClass, fieldLength));
+            return new MvcHtmlString(string.Format("<input type=\"text\" value=\"{0}\" class=\"der-value-{1} form-control der-kpi {8}\"   placeholder=\"{2}\" tabindex=\"{3}\" data-type=\"{4}\" data-kpi-id=\"{5}\" data-id=\"{6}\" data-value-type=\"{7}\" style=\"width: {9}px; text-align: right; \" />", value, existValue, placeholder, tabIndex, type, kpiId, id, valueType, additionalClass, fieldLength));
         }
 
         public class ValueObject {
